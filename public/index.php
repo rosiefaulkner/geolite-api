d4d8552ac056fab9a5f6e150286534441fa800b1
Rosie Faulkner <faulknerproject@gmail.com>
2022-02-27 21:39:24 -0500

    initial adding of DB and user data for sending updates

---
+++ public/index.php
@@ -0,0 +1,31 @@
+<?php
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Selective\BasePath\BasePathMiddleware;
+use Slim\Factory\AppFactory;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../src/config/db.php';
+
+$app = AppFactory::create();
+
+// Add Slim routing middleware
+$app->addRoutingMiddleware();
+
+// Set the base path to run the app in a subdirectory.
+// This path is used in urlFor().
+$app->add(new BasePathMiddleware($app));
+
+$app->addErrorMiddleware(true, true, true);
+
+// Define app routes
+$app->get('/', function (Request $request, Response $response) {
+    $response->getBody()->write('Hello, World!');
+    return $response;
+})->setName('root');
+
+// Customer Routes
+require __DIR__ . '/../src/routes/customers.php';
+
+// Run app
+$app->run();
---
+++ src/routes/customers.php
@@ -0,0 +1,88 @@
+<?php
+
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+use Slim\Factory\AppFactory;
+
+$app = AppFactory::create();
+
+// Get All Customers
+
+$app->get('/api/customers', function (Request $request, Response $response) {
+    $sql = "SELECT * FROM customers";
+
+    try {
+        // Get DB Object
+        $db = new db();
+        // Connect
+        $db = $db->connect();
+
+        $stmt = $db->query($sql);
+        $customers = $stmt->fetchAll((PDO::FETCH_OBJ));
+        $db = null;
+        $response->getBody()->write(json_encode($customers));
+        return $response;
+    } catch (PDOException $e) {
+        echo '{"error": {"text": ' . $e->getMessage() . '}}';
+    }
+});
+
+// Get Single Customer
+
+$app->get('/api/customer/{id}', function (Request $request, Response $response) {
+    $id = $request->getAttribute('id');
+    $sql = "SELECT * FROM customers WHERE id = $id";
+
+    try {
+        // Get DB Object
+        $db = new db();
+        // Connect
+        $db = $db->connect();
+
+        $stmt = $db->query($sql);
+        $customer = $stmt->fetchAll((PDO::FETCH_OBJ));
+        $db = null;
+        $response->getBody()->write(json_encode($customer));
+        return $response;
+    } catch (PDOException $e) {
+        echo '{"error": {"text": ' . $e->getMessage() . '}}';
+    }
+});
+
+// Add Customer
+
+$app->post('/api/customer/add', function (Request $request, Response $response) {
+    $first_name = $request->getParam('first_name');
+    $last_name = $request->getParam('last_name');
+    $phone = $request->getParam('phone');
+    $email = $request->getParam('email');
+    $address = $request->getParam('address');
+    $city = $request->getParam('city');
+    $state = $request->getParam('state');
+
+    $sql = "INSERT INTO customers (first_name, last_name, phone, email, address, city, state) VALUES 
+    (:first_name, :last_name, :phone, :email, :address, :city, :state)";
+
+    try {
+        // Get DB Object
+        $db = new db();
+        // Connect
+        $db = $db->connect();
+
+        $stmt = $db->prepare($sql);
+        $stmt->bindParam(':first_name',     $first_name);
+        $stmt->bindParam(':last_name',      $last_name);
+        $stmt->bindParam(':phone',          $phone);
+        $stmt->bindParam(':email',          $email);
+        $stmt->bindParam(':address',        $address);
+        $stmt->bindParam(':city',           $city);
+        $stmt->bindParam(':state',          $state);
+
+        $stmt->execute();
+        $message = ' Notice: Customer Added';
+        $response->getBody()->write(json_encode($message));
+        return $response;
+    } catch (PDOException $e) {
+        echo '{"error": {"text": ' . $e->getMessage() . '}}';
+    }
+});
